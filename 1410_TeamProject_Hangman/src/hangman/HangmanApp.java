cf998b9b7e00b8f47d9b7deccb857ee4d7f99194
Heath Shurtleff <h2shurtleff@gmail.com>
2022-07-14 18:21:46 -0600

    Adding the first file, just to get it working

---
+++ 1410_TeamProject_Hangman/src/hangman/HangmanApp.java
@@ -0,0 +1,17 @@
+package hangman;
+
+/**
+ * @author hshurtleff
+ *
+ */
+public class HangmanApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
